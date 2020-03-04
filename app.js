28c8f4c9bd137a11d13a3b48d0561d4c544008d5
Bao Tran <btrnaoa@gmail.com>
2020-03-04 14:00:43 +1100

    Retrieve weather data
    * 10m wait in-between API calls
    * Store initial data in local storage (Store.js)
    * Hide API key used in URL param with Express.js and dotenv

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+const axios = require('axios');
+const cors = require('cors');
+const store = require('store');
+
+require('dotenv').config();
+
+app.use(cors());
+
+app.get('/:country/:city', async function(req, res) {
+  let data = {};
+  const dt = store.get('dt');
+  if (dt && Math.floor((Date.now() - dt) / 60000) < 10) {
+    const dataStore = store.get('data');
+    data = dataStore ? dataStore : data;
+  } else {
+    const resp = await axios.get(
+      `http://api.openweathermap.org/data/2.5/forecast?q=${req.params.city},${req.params.country}&appid=${process.env.API_KEY}`
+    );
+    data = await resp.data;
+    store.set('data', data);
+  }
+  store.set('dt', Date.now());
+  res.send(data);
+});
+
+app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
