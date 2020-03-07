bc007f3a5615715eb746af465fdaa38fe4bc844b
Bao Tran <btrnaoa@gmail.com>
2020-03-08 03:05:12 +1100

    Add view for day forecast

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 
+const daysInWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
 class Day extends React.Component {
   render() {
-    return null;
+    const forecast = this.props.forecast;
+    return (
+      <div className="forecast daily">
+        <span>{daysInWeek[forecast.day]}</span>
+        <img
+          src={`https://openweathermap.org/img/wn/${forecast.icon}@2x.png`}
+          alt={forecast.description}
+          title={forecast.description}
+        />
+        <div>
+          <span className="temps high">{Math.floor(forecast.high)}&deg;</span>{' '}
+          <span className="temps low">{Math.floor(forecast.low)}&deg;</span>
+        </div>
+        <div></div>
+      </div>
+    );
   }
 }
 
