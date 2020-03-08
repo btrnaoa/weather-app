962d932183a251f42add89b77da8d175ee2d9f0c
Bao Tran <btrnaoa@gmail.com>
2020-03-09 01:53:27 +1100

    Remove CSS frameworks for vanilla CSS
    * +normalize.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'normalize.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
