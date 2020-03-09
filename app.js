e3524529e6f934b81e162e36f3e59c17310da8d4
Bao Tran <btrnaoa@gmail.com>
2020-03-09 17:34:43 +1100

    Remove API call limit

--- app.js
+++ app.js
@@ -3,26 +3,16 @@ const app = express();
 const port = 3001;
 const axios = require('axios');
 const cors = require('cors');
-const store = require('store');
 
 require('dotenv').config();
 
 app.use(cors());
 
 app.get('/:country/:city', async function(req, res) {
-  let data = {};
-  const dt = store.get('dt');
-  if (dt && Math.floor((Date.now() - dt) / 60000) < 10) {
-    const dataStore = store.get('data');
-    data = dataStore ? dataStore : data;
-  } else {
-    const resp = await axios.get(
-      `http://api.openweathermap.org/data/2.5/forecast?q=${req.params.city},${req.params.country}&appid=${process.env.API_KEY}&units=metric`
-    );
-    data = await resp.data;
-    store.set('data', data);
-  }
-  store.set('dt', Date.now());
+  const resp = await axios.get(
+    `http://api.openweathermap.org/data/2.5/forecast?q=${req.params.city},${req.params.country}&appid=${process.env.API_KEY}&units=metric`
+  );
+  const data = await resp.data;
   res.send(data);
 });
 
