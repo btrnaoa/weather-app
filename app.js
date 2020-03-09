730db9df41b2dc036c5ef91397dce1069e661d70
Bao Tran <btrnaoa@gmail.com>
2020-03-09 18:15:44 +1100

    Remove unncessary async keyword to outer functions
    * Add error logging for making request

--- app.js
+++ app.js
@@ -8,12 +8,17 @@ require('dotenv').config();
 
 app.use(cors());
 
-app.get('/:country/:city', async function(req, res) {
-  const resp = await axios.get(
-    `http://api.openweathermap.org/data/2.5/forecast?q=${req.params.city},${req.params.country}&appid=${process.env.API_KEY}&units=metric`
-  );
-  const data = await resp.data;
-  res.send(data);
+app.get('/:country/:city', function(req, res) {
+  axios
+    .get(
+      `http://api.openweathermap.org/data/2.5/forecast?q=${req.params.city},${req.params.country}&appid=${process.env.API_KEY}&units=metric`
+    )
+    .then(function(resp) {
+      res.send(resp.data);
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
 });
 
 app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
