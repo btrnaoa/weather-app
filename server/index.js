588654cbd08335c3ed9d1b430d3e1b43a3328db7
Bao Tran <btrnaoa@gmail.com>
2020-03-15 11:32:09 +1100

    Priority serve any static files

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const axios = require('axios');
 const cors = require('cors');
@@ -6,9 +7,11 @@ const cors = require('cors');
 if (process.env.NODE_ENV !== 'production')
   require('dotenv').config({ path: './.env.development.local' });
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
+
+app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
 app.get('/:country/:city', (req, res) => {
   axios
