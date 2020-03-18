573756424490bbb538306442040f934f083b57ba
Bao Tran <btrnaoa@gmail.com>
2020-03-18 16:23:28 +1100

    Convert class to stateless function

--- react-ui/src/components/Day.jsx
+++ react-ui/src/components/Day.jsx
@@ -3,27 +3,22 @@ import '../css/weather-icons.min.css';
 
 const daysInWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-class Day extends React.Component {
-  render() {
-    const forecast = this.props.forecast;
-    return (
-      <div className="forecast daily">
-        <span>{daysInWeek[forecast.day]}</span>
-        <i
-          className={`wi wi-owm-${forecast.id}`}
-          style={{ color: '#333', fontSize: '5rem' }}
-          alt=""
-        />
-        <div style={{ fontSize: '1.8rem' }}>
-          <span style={{ color: '#2b2b2b' }}>
-            {Math.floor(forecast.high)}&deg;
-          </span>{' '}
-          <span>{Math.floor(forecast.low)}&deg;</span>
-        </div>
-        <span>{forecast.description}</span>
+export default function Day({ forecast }) {
+  return (
+    <div className="forecast daily">
+      <span>{daysInWeek[forecast.day]}</span>
+      <i
+        className={`wi wi-owm-${forecast.id}`}
+        style={{ color: '#333', fontSize: '5rem' }}
+        alt=""
+      />
+      <div style={{ fontSize: '1.8rem' }}>
+        <span style={{ color: '#2b2b2b' }}>
+          {Math.floor(forecast.high)}&deg;
+        </span>{' '}
+        <span>{Math.floor(forecast.low)}&deg;</span>
       </div>
-    );
-  }
+      <span>{forecast.description}</span>
+    </div>
+  );
 }
-
-export default Day;
